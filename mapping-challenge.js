15b97a565a51c7e4a798264b349b3e96dfb56d4c
Jeffrey Chang <jeffreycj.chang@gmail.com>
2016-11-03 16:12:04 +0000

    Trying out the mapping function

---
+++ mapping-challenge.js
@@ -0,0 +1,24 @@
+var input =
+[
+  { x: 3,
+    y: 4
+  },
+  { x: 12,
+    y: 5
+  },
+  { x: 8,
+    y: 15
+  }
+];
+
+var result = input.map(function(item, index){
+  item.x *= item.x
+  item.y *= item.y
+  var square = Math.sqrt(item.x + item.y)
+  return square;
+})
+
+
+console.log(result[0] === 5);
+console.log(result[1] === 13);
+console.log(result[2] === 17);
